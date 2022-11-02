ce478b979afa70389369d5928da3459b5f778339
Adr0it <46908309+Adr0it@users.noreply.github.com>
2022-11-01 22:47:22 -0500

    Lets you change board components on Chess.com.

---
+++ chess_game_preview_styling.js
@@ -0,0 +1,78 @@
+// ==UserScript==
+// @name         Chess.com Game Preview Styling
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Applies custom styling to Chess.com home page.
+// @author       Adr0it
+// @match        https://www.chess.com/home
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chess.com
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+const imgs = new Map([
+  /* White Pieces */
+  ["wp", ""],
+  ["wn", ""],
+  ["wb", ""],
+  ["wr", ""],
+  ["wq", ""],
+  ["wk", ""],
+
+  /* Black Pieces */
+  ["bp", ""],
+  ["bn", ""],
+  ["bb", ""],
+  ["br", ""],
+  ["bq", ""],
+  ["bk", ""],
+]);
+
+/* Background */
+const background = "";
+
+function waitForElement(querySelector, timeout){
+return new Promise((resolve, reject)=>{
+  var timer = false;
+  if(document.querySelectorAll(querySelector).length) return resolve();
+  const observer = new MutationObserver(()=>{
+    if(document.querySelectorAll(querySelector).length){
+      observer.disconnect();
+      if(timer !== false) clearTimeout(timer);
+      return resolve();
+    }
+  });
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+  if (timeout) timer = setTimeout(()=>{
+    observer.disconnect();
+    reject();
+  }, timeout);
+});
+}
+
+waitForElement("div.game-preview-component.promo-preview", 300).then(function(){
+  let components = document.querySelectorAll("div.game-preview-component.promo-preview");
+  if (components) {
+      var t = setInterval(() => {
+              for (let i = 0; i < components.length; i++) {
+                  if (background != "")
+                      components[i].style.backgroundImage = "url(" + background + ")";
+
+                  let pieces = components[i].children;
+                  for (let j = 0; j < pieces.length; j++) {
+                      let curPiece = pieces[j];
+                      let key = curPiece.src[curPiece.src.indexOf(".png") - 2] + curPiece.src[curPiece.src.indexOf(".png") - 1];
+                      if (imgs.get(key) != "")
+                          curPiece.src = imgs.get(key);
+                  }
+              }
+          clearInterval(t);
+      }, 300);
+
+  }
+}).catch(()=>{
+ console.log("failed");
+});
